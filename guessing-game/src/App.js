884932d6660ded57288ef42e2587b38bfc8134b2
Zhenya <alexanyan.jenya@gmail.com>
2023-11-19 16:31:33 +0400

    code without styles

--- guessing-game/src/App.js
+++ guessing-game/src/App.js
@@ -1,56 +1,65 @@
 import { useEffect, useState } from "react";
+import './App.css';
 
 function App() {
   
   const APIKey = "793d85827e162e26a65fde3fa1dd7975";
-  const [guess, setGuess] = useState('');
-  const [cityIndex, setCityIndex] = useState(0);
+  const city = ["London", "Yerevan", "Seoul", "New York", "Paris"];
+  let result = 0;
+
+  const [apiData, setApiData] = useState();
   const [posts, setPosts] = useState([]);
+  let [guess, setGuess] = useState('');
+  let [cityIndex, setCityIndex] = useState(0);
 
   useEffect(() => {
-    const city = ["London", "Yerevan", "Seoul", "New York", "Paris"][cityIndex];
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=793d85827e162e26a65fde3fa1dd7975`)
+    const currentCity = city[cityIndex];
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${currentCity}&units=metric&appid=${APIKey}`)
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
-        setGuess(data.main.temp); // assuming you want to store the temperature as the guess
+        setApiData(data.main.temp); 
       })
       .catch((err) => {
-        console.log(err.message);
+        console.error(err.message);
       });
-  }, [cityIndex]);
+  });
 
-  const addPosts = async (temp) => {
-    const city = ["London", "Yerevan", "Seoul", "New York", "Paris"][cityIndex];
-    await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`, {
-      method: 'POST',
-      body: JSON.stringify({
-        temp: temp,
-      }),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setPosts((posts) => [...posts, { guess: temp, temp: data.main.temp }]);
-        setGuess('');
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  };
+  const addPosts = () => { 
+    if(apiData + 5 >= parseFloat(guess) && apiData - 5 <= parseFloat(guess)){ 
+      setPosts((posts) => [...posts, { guess, temp: apiData, isValid: true }]);
+      result++;
+    }else{ 
+      setPosts((posts) => [...posts, { guess, temp: apiData, isValid: false }]); 
+    } 
+    setCityIndex(++cityIndex);
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    addPosts(guess);
+    addPosts();
   };
+
+  if (cityIndex === 5) {
+    if (result >= 2) {
+      return (
+        <div>
+          <span>You win</span>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <span>You lost</span>
+        </div>
+      );
+    }
+  }
 
   return (
     <div className="app">
       <div>
         <h1>Main area box</h1>
-        <p className="city-name"></p>
+        <p className="city-name">{city[cityIndex]}</p>
         <input
           type="text"
           onChange={(e) => setGuess(e.target.value)}
@@ -62,10 +71,19 @@ function App() {
         </button>
       </div>
       <div className="guesses">
-        {posts.map((post, index) => (
+      {posts.map((post, index) => (
           <div key={index}>
-            <span className="guess-temp">{post.guess}</span>
-            <span className="correct-temp">Was {post.temp}</span>
+            {post.isValid ? (
+              <>
+              <span className="guess-temp correct">{post.guess}</span>
+              <span className="correct-temp correct">Was {post.temp}</span>
+              </>
+            ) : (
+              <>
+              <span className="guess-temp wrong">{post.guess}</span>
+              <span className="correct-temp wrong">Was {post.temp}</span>
+              </>
+            )}
           </div>
         ))}
       </div>
--- guessing-game/src/components/verification/verification.js
+++
@@ -1,9 +0,0 @@
-function verification(inputValue) {
-    if (inputValue.value === "") {
-        alert("The guess area should not be empty!!");
-        return false;
-    }
-    return true;
-}
-
-export default verification;
